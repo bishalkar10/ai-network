7f7e07f10faa11676a42195180ac2d29ce26a265
Bishal Kar <bishalkar10@gmail.com>
2023-08-27 09:11:45 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className='bg-red-500'>AI Network</h1>
+  )
+}
+
+export default App
