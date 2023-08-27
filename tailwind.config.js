c1d2e9cf181bb6f1152b8f431b4fde7ffee46d99
Bishal Kar <bishalkar10@gmail.com>
2023-08-27 14:26:49 +0530

    added the custom animation 'expand' and 'collapse'

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    keyframes: {
+      expand: {
+        "0%": { height: "0" },
+        "100%": { height: "200px" },
+      },
+      collapse: {
+        "0%": { height: "200px" },
+        "100%": { height: "0" },
+      },
+    },
+    animation: {
+      expand: "expand 0.3s ease-in-out forwards",
+      collapse: "collapse 0.3s ease-in-out forwards",
+    },
   },
   plugins: [],
 };
