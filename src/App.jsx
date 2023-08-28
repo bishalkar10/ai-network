945390d554f7b856c81aed84b2454984d92d6d55
Bishal Kar <112653391+bishalkar10@users.noreply.github.com>
2023-08-28 13:21:13 +0000

    added background color to the app

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
+import Contact from "./components/Contact";
+
 function App() {
   return (
-    <>
+    <div className="bg-[#060A14]">
       <Navbar />
       <Homepage />
       <Services />
+      <Contact />
       {/* <About />
       <Roadmap /> */}
-    </>
+    </div>
   );
 }
 
