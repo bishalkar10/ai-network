1598b566480b887548eee14631824087cbf1a6db
Bishal Kar <112653391+bishalkar10@users.noreply.github.com>
2023-08-28 13:30:28 +0000

    changed padding left and right & top level div to section
    - padding left and right is now 12px
    - padding top is removed and padding bottom is 40px

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -56,13 +56,13 @@ export default function Services() {
     />
   ));
   return (
-    <div className="bg-[#060A14] px-[30px] w-full min-h-screen py-10">
-      <h2 className="text-center text-white text-3xl uppercase pb-10">
+    <section className="bg-[#060A14] px-3 pb-10 w-full min-h-screen">
+      <h2 className="text-center text-white text-3xl uppercase py-10 font-bold">
         Our Services
       </h2>
       <div className="auto-rows-min h-full mx-auto bp:w-[540px] md:w-[720px] bp2:w-[960px] grid grid-cols-1 bp:grid-cols-2 bp2:grid-cols-3 gap-6">
         {cardList}
       </div>
-    </div>
+    </section>
   );
 }
