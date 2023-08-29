e4f50549639cb57f467ea9509670c8ec0b5e8d4f
Bishal Kar <112653391+bishalkar10@users.noreply.github.com>
2023-08-29 11:33:10 +0000

    increased font size and added Inter font style to a text

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -14,13 +14,13 @@ export default function Homepage() {
       className='bg-[url("ai-home-image.jpg")] bg-cover bg-center min-h-screen relative px-3 flex items-center'
     >
       <div className="md:pl-10">
-        <h2 className="text-white text-4xl md:text-5xl font-bold md:w-[460px]">
+        <h2 className="text-white text-4xl md:text-[55px] md:leading-[3.5rem] font-bold md:w-[460px]">
           AI and Robotics can change your
           <span className="ml-5 inline-block bg-[#59C09D] mt-5 py-1 px-5 rounded-[15px] text-[#060a14] uppercase">
             Thinking
           </span>
         </h2>
-        <ul className="flex text-white gap-5 mt-5">
+        <ul className="flex flex-wrap text-white gap-5 mt-5">
           <li>
             <a
               className="hover:text-[#F6AE64] transition-all ease duration-500"
@@ -58,7 +58,7 @@ export default function Homepage() {
           </li>
         </ul>
         <div className="mt-[30px] p-[25px] bg-[rgba(6,10,20,0.5)] max-w-[730px] rounded-[15px] md:max-w-[360px] md:absolute md:bottom-0 md:right-0">
-          <p className="text-white">
+          <p className="text-white font-Inter">
             AI and robotic technologies have gained significant traction across
             industries, revolutionizing the way work is performed, improving
             efficiency and increasing safety.
