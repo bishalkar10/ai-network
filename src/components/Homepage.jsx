c826d2dba78d22d9b03e5d823bebde648f4718f8
Bishal Kar <112653391+bishalkar10@users.noreply.github.com>
2023-08-30 13:56:09 +0000

    added font-style Exo to the text

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -14,7 +14,7 @@ export default function Homepage() {
       className='bg-[url("ai-home-image.jpg")] bg-cover bg-center min-h-screen relative px-3 flex items-center'
     >
       <div className="md:pl-10">
-        <h2 className="text-white text-4xl md:text-[55px] md:leading-[3.5rem] font-bold md:w-[460px]">
+        <h2 className="text-white font-Exo text-4xl md:text-[55px] md:leading-[3.5rem] font-bold md:w-[460px]">
           AI and Robotics can change your
           <span className="ml-5 inline-block bg-[#59C09D] mt-5 py-1 px-5 rounded-[15px] text-[#060a14] uppercase">
             Thinking
@@ -59,8 +59,9 @@ export default function Homepage() {
         </ul>
         <div className="mt-[30px] p-[25px] bg-[rgba(6,10,20,0.5)] max-w-[730px] rounded-[15px] md:max-w-[360px] md:absolute md:bottom-0 md:right-0">
           <p className="text-white font-Inter">
-        
-"Empowering the Future: Where AI Meets Blockchain - The AI Network Project Unveils a Seamless Ecosystem for Trading, Development, and Content Creation"
+            "Empowering the Future: Where AI Meets Blockchain - The AI Network
+            Project Unveils a Seamless Ecosystem for Trading, Development, and
+            Content Creation"
           </p>
         </div>
       </div>
